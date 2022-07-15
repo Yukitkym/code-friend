3f444b3e693e2464dd206ac1a8f8d50ecd9c0239
YukiTakayama <yukitkym1011@gmail.com>
2022-07-15 15:29:03 +0900

    #30 投稿者とログインユーザーが一致した場合のみ、投稿編集ページへのリンクが表示されるようにする

--- src/pages/posts/[id]/index.tsx
+++ src/pages/posts/[id]/index.tsx
@@ -1,27 +1,20 @@
 import { doc, getDoc } from "firebase/firestore"
 import Link from "next/link"
 import { useRouter } from "next/router"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { useRecoilValue } from "recoil"
-import { isLoginState, uidState } from "../../../atoms"
+import { uidState } from "../../../atoms"
 import { db } from "../../../firebaseConfig"
 
 export default function PostsId() {
   const router = useRouter()
+  const postId = router.asPath.slice(7)
 
-  const isLogin = useRecoilValue(isLoginState)
   const uid = useRecoilValue(uidState)
 
-  useEffect(() => {
-    if (isLogin === false) {
-      router.push("/")
-    }
-  }, [isLogin])
-
-  const [post, setPost] = useState({id: "", title: "", content: ""})
+  const [post, setPost] = useState({poster: "", title: "", content: ""})
 
   const getPost = async () => {
-    const postId = router.asPath.slice(7)
     const postRef = doc(db, "posts", postId)
     const postSnap = await getDoc(postRef)
     if (postSnap.exists()) {
@@ -34,12 +27,15 @@ export default function PostsId() {
     <div>
       <p>投稿詳細ページ</p>
       <br />
-      <p>{post.id}</p>
+      <p>{postId}</p>
       <p>{post.title}</p>
       <p>{post.content}</p>
-      <Link href={`/posts/${post.id}/edit`}>
-        <p>投稿編集ページへ</p>
-      </Link>
+      <p>{post.poster}</p>
+      {post.poster === uid && (
+        <Link href={`/posts/${postId}/edit`}>
+          <p>投稿編集ページへ</p>
+        </Link>
+      ) }
     </div>
   )
 }
