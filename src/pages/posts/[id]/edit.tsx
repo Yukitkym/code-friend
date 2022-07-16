fdf979d151411adfcd914a241f35050388ac0ad6
YukiTakayama <yukitkym1011@gmail.com>
2022-07-16 19:57:35 +0900

    #36 投稿編集時にサンプル画像から選べるように

--- src/pages/posts/[id]/edit.tsx
+++ src/pages/posts/[id]/edit.tsx
@@ -37,6 +37,8 @@ export default function PostsIdEdit() {
   const [post, setPost] = useState({id: "", title: "", content: "", image: ""})
   const [posts, setPosts] = useState([{id: "", title: "", content: ""}])
   const [postNum, setPostNum] = useState(0)
+  const [selectImage, setSelectImage] = useState("not change")
+  const [choiceImage, setChoiceImage] = useState("example1")
 
   const getUserPost = async () => {
     const userRef = doc(db, "users", uid)
@@ -54,21 +56,23 @@ export default function PostsIdEdit() {
   getUserPost()
 
   const clickEditDone = async  () => {
-    const image = document.getElementById("image") as HTMLInputElement
-    if (image.value !== "") {
-      // 画像をStorageに保存
-      await uploadBytes(ref(storage, `postImages/${post.id}`), image.files[0])
-      const pathReference = ref(storage, `postImages/${post.id}`)
-      let imageUrl = ""
-      await getDownloadURL(pathReference)
-      .then((url) => {
-        imageUrl = url
+    let imageUrl = ""
+    if (selectImage === "not change") {
+      // 画像は変更しないver
+      const newPosts = posts.map((oldPost) => {
+        return oldPost.id === post.id ? post : oldPost
       })
-      await updateDoc(doc(db, 'posts', post.id), {
+      await updateDoc(doc(db, "users", uid), {
+        "posts": newPosts
+      })
+      await updateDoc(doc(db, "posts", postId), {
         "title": post.title,
-        "content": post.content,
-        "image": imageUrl
+        "content": post.content
       })
+
+    } else if (selectImage === "choice") {
+      // サンプル画像ver
+      imageUrl = document.getElementById(choiceImage)!.src
       const newPosts = posts.map((oldPost) => {
         return oldPost.id === post.id ? {
           "id": post.id,
@@ -80,17 +84,50 @@ export default function PostsIdEdit() {
       await updateDoc(doc(db, "users", uid), {
         "posts": newPosts
       })
-    } else {
-      const newPosts = posts.map((oldPost) => {
-        return oldPost.id === post.id ? post : oldPost
-      })
-      await updateDoc(doc(db, "users", uid), {
-        "posts": newPosts
-      })
-      await updateDoc(doc(db, "posts", postId), {
+      await updateDoc(doc(db, 'posts', post.id), {
         "title": post.title,
-        "content": post.content
+        "content": post.content,
+        "image": imageUrl
       })
+    } else {
+      //　アップロードされた画像ver
+      const image = document.getElementById("image") as HTMLInputElement
+      if (image.value !== "") {
+        // 画像をStorageに保存
+        await uploadBytes(ref(storage, `postImages/${post.id}`), image.files[0])
+        const pathReference = ref(storage, `postImages/${post.id}`)
+        await getDownloadURL(pathReference)
+        .then((url) => {
+          imageUrl = url
+        })
+        await updateDoc(doc(db, 'posts', post.id), {
+          "title": post.title,
+          "content": post.content,
+          "image": imageUrl
+        })
+        const newPosts = posts.map((oldPost) => {
+          return oldPost.id === post.id ? {
+            "id": post.id,
+            "title": post.title,
+            "content": post.content,
+            "image": imageUrl
+        } : oldPost
+        })
+        await updateDoc(doc(db, "users", uid), {
+          "posts": newPosts
+        })
+      } else {
+        const newPosts = posts.map((oldPost) => {
+          return oldPost.id === post.id ? post : oldPost
+        })
+        await updateDoc(doc(db, "users", uid), {
+          "posts": newPosts
+        })
+        await updateDoc(doc(db, "posts", postId), {
+          "title": post.title,
+          "content": post.content
+        })
+      }
     }
     router.push(`/posts/${post.id}`)
   }
@@ -126,8 +163,26 @@ export default function PostsIdEdit() {
       <br />
       <img src={post.image} className="w-[300px]" />
       <br />
-      <input id="image" type="file" />
+      <select name="selectImage" id="selectImage" value={selectImage} onChange={(e) => setSelectImage(e.target.value)}>
+        <option value="not change">現在の画像を使う</option>
+        <option value="choice">サンプル画像から選ぶ</option>
+        <option value="upload">画像をアップロードする</option>
+      </select>
       <br />
+      <br />
+      <input id="image" type="file" disabled={selectImage === "not change" || selectImage === "choice"} />
+      <br />
+      <br />
+      {selectImage === "choice" && (
+        <div className='flex'>
+          <img src="https://firebasestorage.googleapis.com/v0/b/code-friend.appspot.com/o/postImages%2Fexample1.jpg?alt=media&token=6bb2265e-27fd-4153-a8f4-52fbd1e0ee0f" alt="例1" id="example1" className={`w-[300px] mr-[30px] p-[5px] ${choiceImage === "example1" ? 'bg-code-blue' : 'bg-white'}`} onClick={() => setChoiceImage("example1")} />
+          <img src="https://firebasestorage.googleapis.com/v0/b/code-friend.appspot.com/o/postImages%2Fexample2.jpg?alt=media&token=eb9e6e66-5bbe-4cca-94b5-53268bcc78d5" alt="例2" id="example2" className={`w-[300px] mr-[30px] p-[5px] ${choiceImage === "example2" ? 'bg-code-blue' : 'bg-white'}`} onClick={() => setChoiceImage("example2")} />
+          <img src="https://firebasestorage.googleapis.com/v0/b/code-friend.appspot.com/o/postImages%2Fexample3.jpg?alt=media&token=918e08bb-ac56-4ba6-aa1a-37d5dee38958" alt="例3" id="example3" className={`w-[300px] mr-[30px] p-[5px] ${choiceImage === "example3" ? 'bg-code-blue' : 'bg-white'}`} onClick={() => setChoiceImage("example3")} />
+          <img src="https://firebasestorage.googleapis.com/v0/b/code-friend.appspot.com/o/postImages%2Fexample4.jpg?alt=media&token=0fe201b0-af92-44df-9df4-961b7f8d7b36" alt="例4" id="example4" className={`w-[300px] mr-[30px] p-[5px] ${choiceImage === "example4" ? 'bg-code-blue' : 'bg-white'}`} onClick={() => setChoiceImage("example4")} />
+          <img src="https://firebasestorage.googleapis.com/v0/b/code-friend.appspot.com/o/postImages%2Fexample5.jpg?alt=media&token=3ccdfcea-10ef-42f8-baf2-c02462724e11" alt="例5" id="example5" className={`w-[300px] mr-[30px] p-[5px] ${choiceImage === "example5" ? 'bg-code-blue' : 'bg-white'}`} onClick={() => setChoiceImage("example5")} />
+          <img src="https://firebasestorage.googleapis.com/v0/b/code-friend.appspot.com/o/postImages%2Fexample6.jpg?alt=media&token=ca652961-f177-4c66-88d0-87618a0d89eb" alt="例6" id="example6" className={`w-[300px] mr-[30px] p-[5px] ${choiceImage === "example6" ? 'bg-code-blue' : 'bg-white'}`} onClick={() => setChoiceImage("example6")} />
+        </div>
+      )}
       <br />
       <button onClick={clickEditDone}>編集完了</button>
       <br />
