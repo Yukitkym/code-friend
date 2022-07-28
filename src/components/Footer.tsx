3a94586aa4317a1706b3d72e32057e1d5ff1fd58
YukiTakayama <yukitkym1011@gmail.com>
2022-07-28 20:54:35 +0900

    #78 TopページとHeaderとFooterをレスポンシブ対応

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
-    <div className="h-[8vh] bg-black-light flex">
-      <p className="text-code-white m-auto">Copyright©︎ Takayama All Rights Reserved.</p>
+    <div className="h-[6vh] lg:h-[8vh] bg-black-light flex">
+      <p className="text-[14px] lg:text-[16px] text-code-white m-auto">Copyright©︎ Takayama All Rights Reserved.</p>
     </div>
   )
 }
