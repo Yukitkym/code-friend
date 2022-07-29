4a342046644c76d952bd412152f1dac37ee43507
YukiTakayama <yukitkym1011@gmail.com>
2022-07-29 15:43:36 +0900

    #78 投稿詳細ページをレスポンシブ対応

--- src/pages/posts/[id]/index.tsx
+++ src/pages/posts/[id]/index.tsx
@@ -40,14 +40,14 @@ export default function PostsId() {
 
   if (user.image !== '') {
     return (
-      <div className="bg-bg-color text-code-white pb-[40px]">
-        <h1 className="text-center text-[24px] py-[20px]">投稿詳細ページ</h1>
-        <div className="w-[540px] mx-auto bg-bg-light-color border-[#000078] border-[1px] border-opacity-10">
-          <div className="mx-[60px] my-[40px]">
-            <div className="relative h-[280px] w-[420px] mx-auto">
+      <div className="min-h-[88vh] lg:min-h-[84vh] bg-bg-color text-code-white pb-[40px]">
+        <h1 className="lg:text-[24px] pt-[15px] lg:py-[20px] text-center">投稿詳細ページ</h1>
+        <div className="lg:w-[540px] mx-auto bg-bg-light-color border-[#000078] border-[1px] border-opacity-10">
+          <div className="text-[14px] lg:text-[16px] mx-[15px] lg:mx-[60px] my-[15px] lg:my-[40px]">
+            <div className="w-[270px] lg:w-[420px] h-[180px] lg:h-[280px] relative mx-auto">
               <Image src={post.image} alt="投稿サムネイル画像" layout="fill" />
               <div className="absolute bg-black opacity-[40%] w-[100%] h-[100%]"></div>
-              <p className="text-code-white absolute text-[30px] mx-[10px] mt-[15px]">{post.title}</p>
+              <p className="text-[20px] lg:text-[30px] mx-[10px] mt-[15px] text-code-white absolute">{post.title}</p>
             </div>
             {post.content !== '' && (
               <div className="bg-zinc-700 w-[100%] rounded mt-[10px]">
@@ -66,11 +66,11 @@ export default function PostsId() {
               </Link>
             )}
             <div className="mt-[30px]">
-              <p className="text-[20px]">投稿ユーザー</p>
+              <p className="text-[16px] lg:text-[20px]">投稿ユーザー</p>
               <Link href={`/user/${post.poster}`}>
                 <div className="flex mt-[10px]">
                   <Image src={user.image} alt="プロフィール画像" width="60px" height="60px" className="rounded-full" />
-                  <p className="my-auto ml-[10px] text-[20px]">{user.name}</p>
+                  <p className="text-[16px] lg:text-[20px] my-auto ml-[10px]">{user.name}</p>
                 </div>
               </Link>
               <p className="mt-[15px]">言語</p>
